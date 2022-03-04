9aa3379f148856972b32d1ad272227cea3575d1e
Tunisia Artope <artope@yahoo.com>
2022-03-04 14:58:51 -0500

    completed assignment

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,22 @@
 import React from "react";
+import BotCard from "./BotCard"
 
-function BotCollection() {
+function BotCollection({ bots, botArmy, setBotArmy }) {
   // Your code here
+
+  const botCard = bots.map(bot =>{
+    function handleClickCard(bot){
+      if(!botArmy.includes(bot)){
+        setBotArmy([...botArmy, bot])
+      }
+    }
+    return <BotCard  key={bot.id} bot={bot} handleClickCard={handleClickCard} />
+  })
+
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {botCard}
         Collection of all bots
       </div>
     </div>
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,13 +1,20 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
+function YourBotArmy({ bots, botArmy, setBotArmy }) {
   //your bot army code here...
-
+  const botArmyCard = bots.map(bot =>{
+    function handleClickCard(){
+      const removeBotArmy = bots.filter((item) => item !== bot)
+      setBotArmy(removeBotArmy)
+    }
+    return <BotCard key={bot.id} bot={bot} handleClickCard={handleClickCard}/>
+  })
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
+          {botArmyCard}
           Your Bot Army
         </div>
       </div>
